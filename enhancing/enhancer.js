d31be1fdccc350ef23902172903938fc09490fc9
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2021-02-24 20:14:22 -0500

    added tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,43 @@ module.exports = {
   success,
   fail,
   repair,
-  get,
+  get
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancement >= 20) {
+    return { ...item,  };
+  }else {
+    return{...item, enhancement: item.enhancement +1}
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement > 16) {
+    return { ...item, enhancement: item.enhancement - 1 };
+  }else if (item.enhancement > 15) {
+    return { ...item, durability: item.durability - 10}
+  }else{
+    return { ...item, durability: item.durability - 5 }
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability:100 };
 }
 
 function get(item) {
-  return { ...item };
+  if(item.enhancement > 0){
+    return { ...item,name: `[+${item.enhancement}]${item.name}` };
+  }else{
+    return{ ...item }
+  }
 }
+
+// class Item {
+//   constructor(name,durability,enhancement){
+//     this.name = name,
+//     this.durability = durability,
+//     this.enhancement = enhancement
+//   }
+// }
